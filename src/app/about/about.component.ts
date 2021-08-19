2bd6da7a336da82c0d2ebb11d0d6099f2554a80a
Chris <mendozac@udel.edu>
2021-08-18 21:18:59 -0400

    Finished about page

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
-@Component({ templateUrl: 'about.component.html' })
-export class AboutComponent {}
+@Component({ 
+    selector: 'app-about',
+    templateUrl: 'about.component.html',
+    styleUrls: ['./about.component.css']
+})
+export class AboutComponent implements OnInit {
+
+    constructor() { }
+
+    description: string = "Some of my interests (outside of coding) include working out, cooking, and longboarding";
+  
+    ngOnInit(): void {
+    }
+  
+  }
--- src/app/projects-data/projects-data.component.ts
+++ src/app/projects-data/projects-data.component.ts
@@ -29,35 +29,18 @@ export class ProjectsDataComponent implements OnInit {
   ];
   descriptionArr: string[] = [
     "This decentralized vaccine record keeper/tracker stores its data on the Ethereum Blockchain.",
-    "This project helped me learn about utilizing SQL statements in a java program. I used JavaFX to create the GUI which I most utilized Scene Builder to generate the components",
+    "This project helped me learn about executing SQL statements in a java program. I used JavaFX to create the GUI and I most utilized Scene Builder to generate the components",
     "I programmed a microprocessor board with servo motors attached to follow a line of black tape.",
     "Funnily enough, I had to do a Sudoku Solver for class the semester after making this. Sadly, it was in Java while I did this in Python.",
     "This project was for a Computer Engineering class. We designed an instruction-fed calculator in VHDL at the RTL level. It's also capable of skipping instructions",
     "Source code for this website!"
   ];
 
-    descriptionArray: string[] = [
-        "This project helped me learn about utilizing SQL statements in a java program. I used JavaFX to create the GUI which I most utilized Scene Builder to generate the components",
-        "Funnily enough, I had to do a Sudoku Solver for class the semester after making this. Sadly, it was in Java while I did this in Python.",
-        "This project was for a Computer Engineering class. We designed an instruction-fed calculator in VHDL with addition/subtraction capabilities at the RTL level. It's also capable of skipping instructions"
-    ];
-
     mouseEnter(div : number){
         this.techStack = this.techStackArr[div];
         this.description = this.descriptionArr[div];
-        if(div == 0) {
-            this.foodDesc = this.descriptionArray[div];
-        }
-        if(div == 1) {
-            this.sudokuDesc = this.descriptionArray[div];
-        }
-        if(div == 2) {
-            this.thirdDesc = this.descriptionArray[div];
-        }
         document.getElementById("scroll")?.setAttribute("style","height: 640px;");
         document.getElementById("scroll2")?.setAttribute("style","height: 640px;");
-        //document.getElementById("scroll")?.setAttribute("style","overflow-x: hidden;");
-        //document.getElementById("scroll2")?.setAttribute("style","overflow-x: hidden;");
      }
   
      mouseLeave(){
