8e8c0b4601a5f66bff89e92fadf0983f8e755de0
Chris <mendozac@udel.edu>
2021-08-22 03:10:26 -0400

    fixed about page, added hover features for experience page

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -8,9 +8,9 @@ import { Component, OnInit } from '@angular/core';
 export class AboutComponent implements OnInit {
 
     constructor() { }
-
-    description: string = "Some of my interests (outside of coding) include working out, cooking, and longboarding";
-  
+    p1: string = "I was interested in technology from a young age, as a kid I would browse the internet while my parents were at work. In hindsight, I probably needed more supervision.";
+    p2: string = "I was born in DuBois, Pennsylvania, and I currently live in Selden, New York. In ten years I see myself working in a managerial position, either that or a senior software engineer position. I plan on staying on the east coast, that way I can stay relatively close to family and New York pizza.";
+    p3: string = "Some of my interests (outside of coding) include working out, cooking, and longboarding. Something I'm trying to improve on is my sleep schedule, I'm a real night owl sometimes. I'm always willing to work on myself, improvement is my biggest motivation. I want to always be better than the person I was yesterday.";
     ngOnInit(): void {
     }
   
--- src/app/timeline/timeline.component.ts
+++ src/app/timeline/timeline.component.ts
@@ -12,4 +12,32 @@ export class TimelineComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  description: string[] = ["Skills Used: ", "Skills Used: ", "Skills Used: ", "Skills Used: ", "Skills Used: ", "Skills Used: "];
+  list: string[] = ["Agile Methodology, C#, Angular, Solidity, Ethereum Blockchain Development", "Java, JavaFX, Maven", "C, PIC32MX Microprocessors, MPLAB IDE", "Tableau, Excel (Pivot Tables)", "VHDL, Vivado, VMWare Workstation", "Excel, Daily Work Reports"];
+  skills: string[] = ["Agile Methodology, C#, Angular, Solidity, Ethereum Blockchain Development", "Java, JavaFX, Maven", "C, PIC32MX Microprocessors, MPLAB IDE", "Tableau, Excel (Pivot Tables)", "VHDL, Vivado, VMWare Workstation", "Excel, Daily Work Reports"];
+  desc: string[] = [
+    "I worked on a .NET application that helped identify and record migration blockers for M&T's databases. I also competed in a summer-long app-dev contest and won!",
+    "I helped students learn the fundamentals of object-oriented program as well as the fundamentals of JavaFX.",
+    "I helped students learn how to program PIC32 microcontrollers. Some key concepts include Interrupt Service Routines and port structure.",
+    "I worked with data analysts to gather data insights on numerous pipeline KPI's.",
+    "I helped students learn the fundamentals of FPGA's and VHDL programming. I also helped them learn about logic gates and boolean algebra.",
+    "I assisted in the collection of construction metrics to develop daily work reports for the accurate payment of construction jobs."
+  ]
+  mouseEnter(div : number){
+    this.description[div] = "Description: ";
+    this.list[div] = this.desc[div];
+ }
+
+ mouseLeave(){
+   //this.description[0] = "Skills Used";
+   this.description.forEach(function(part, index, arr) {
+    arr[index] = "Skills Used: ";
+   }, this.description);
+   var index = 0;
+   for(var v in this.list) {
+     this.list[index] = this.skills[index];
+     index++;
+   }
+ }
+
 }
